a9ba91cef55b3d6bc8e7440dce3acd7617b1f999
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-08-28 15:26:23 +0500

    Create Q4.asm

---
+++ Lab Task 1/Q4.asm
@@ -0,0 +1,23 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Q: A hardware counter records values: 11010110b – 9C4h + 220 + 18 + 1011110b – D + 12.
+	Write Assembly code to calculate the result.
+!
+
+INCLUDE Irvine32.inc
+
+.code
+main PROC
+	mov eax, 11010110b
+	sub eax, 9C4h
+	add eax, 220
+	add eax, 18
+	add eax, 1011110b
+	sub eax, 0Dh
+	add eax, 12
+
+	call WriteInt
+
+	exit
+main ENDP
+END main
