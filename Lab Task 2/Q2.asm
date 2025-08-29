7b7c155a9211e291329e86a670e448d79fab52bc
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-08-30 01:48:57 +0500

    Create Q2.asm

---
+++ Lab Task 2/Q2.asm
@@ -0,0 +1,22 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Q: Assume all registers (EAX, EBX, ECX, EDX) initially hold some values.
+	   EAX Calculation: In a simulation, compute: eax = 4C2h – ebx + 72o + 55d – 11101011b + 180
+!
+
+INCLUDE Irvine32.inc
+
+.code
+main PROC
+	mov eax, 4C2h
+	sub eax, ebx
+	add eax, 72o
+	add eax, 55
+	sub eax, 11101011b
+	add eax, 180
+
+	call WriteInt
+
+	exit
+main ENDP
+END main
