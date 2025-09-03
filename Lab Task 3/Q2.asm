3e9309e7ff29eae98a208f60091aaa156f80af21
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-09-04 01:05:28 +0500

    Create Q2.asm

---
+++ Lab Task 3/Q2.asm
@@ -0,0 +1,18 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Define a string variable called message initialized with "Hello World".
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	message BYTE "Hello World", 0
+
+.code
+main PROC
+	mov edx, OFFSET message
+	call WriteString
+
+	exit
+main ENDP
+END main
