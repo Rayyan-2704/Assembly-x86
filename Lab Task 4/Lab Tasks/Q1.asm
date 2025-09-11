1b43bc71dc789ecfb94e5495db70fea59bc21bec
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-09-12 03:35:51 +0500

    Create Q1.asm

---
+++ Lab Task 4/Lab Tasks/Q1.asm
@@ -0,0 +1,31 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Task 1: Write a program that declares the following variables:
+	- A byte variable num1 initialized with 25.
+	- A word variable num2 initialized with 1200h.
+	- A doubleword variable num3 uninitialized.
+	Move the contents of num1 into AL, num2 into AX, and finally move both into num3 using `MOVZX` instruction.
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	num1 BYTE 25
+	num2 WORD 1200h
+	num3 DWORD ?
+
+.code
+main PROC
+	mov al, num1
+	mov ax, num2
+
+	movzx eax, num1
+	mov num3, eax
+
+	movzx ebx, num2
+	mov num3, ebx
+
+	call DumpRegs
+	exit
+main ENDP
+END main
