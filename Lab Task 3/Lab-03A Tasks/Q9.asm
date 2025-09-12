10972c9a7458c3a899406b3f993f63441c5d6c21
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-09-13 02:55:53 +0500

    Create Q9.asm

---
+++ Lab Task 3/Lab-03A Tasks/Q9.asm
@@ -0,0 +1,16 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Task 9: Declare an array of 20 unsigned words named uArray. Initialize all elements with the value 5.
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	uArray WORD 20 DUP(5)
+
+.code
+main PROC
+
+	exit
+main ENDP
+END main
