5f0b7f47be7f3f2f799ac2d94cd968f1692db631
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-09-13 02:42:22 +0500

    Create Q6.asm

---
+++ Lab Task 3/Lab-03A Tasks/Q6.asm
@@ -0,0 +1,18 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Task 6: FAST NUCES university database system needs to store marks of 5 students in 3 different
+	subjects (Math, Programming, and Database). Declare a two-dimensional array in Assembly
+	language to store this data.
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	marks WORD 5 DUP(3 DUP(?))
+
+.code
+main PROC
+
+	exit
+main ENDP
+END main
