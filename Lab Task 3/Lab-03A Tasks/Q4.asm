513fa33fcc7bdc5e84fa4485099b4eae6c88e794
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-09-13 02:34:37 +0500

    Create Q4.asm

---
+++ Lab Task 3/Lab-03A Tasks/Q4.asm
@@ -0,0 +1,16 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Task 4: Declare an uninitialized array of 30 unsigned doublewords named dArray.
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	dArray DWORD 30 DUP(?)
+
+.code
+main PROC
+
+	exit
+main ENDP
+END main
