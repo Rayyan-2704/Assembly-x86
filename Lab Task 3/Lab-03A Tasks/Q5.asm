6af00df2cfdee8c3ef001ff5e1c9ffea26fa92fb
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-09-13 02:38:34 +0500

    Create Q5.asm

---
+++ Lab Task 3/Lab-03A Tasks/Q5.asm
@@ -0,0 +1,16 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Task 5: Declare an unsigned 16-bit integer variable named that uses three Initializers.
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	nums WORD 100,200,300
+
+.code
+main PROC
+
+	exit
+main ENDP
+END main
