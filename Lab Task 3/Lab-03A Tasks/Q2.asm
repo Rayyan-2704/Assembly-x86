60c8bdd695666951e960ce90a0740e36fa6e39e3
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-09-13 02:25:36 +0500

    Create Q2.asm

---
+++ Lab Task 3/Lab-03A Tasks/Q2.asm
@@ -0,0 +1,16 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Task 2: Declare an array of 10 unsigned bytes named bArray and initialize all elements.
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	bArray BYTE 1,2,3,4,5,6,7,8,9,10
+
+.code
+main PROC
+
+	exit
+main ENDP
+END main
