cc1570c53b1adebbd04722198f18224ca016d97e
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-09-13 02:46:31 +0500

    Create Q7.asm

---
+++ Lab Task 3/Lab-03A Tasks/Q7.asm
@@ -0,0 +1,17 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Task 7: Karachi Bakery sales system records the daily sales of 7 days. Declare an array of signed
+	integers to store sales data. Initialize it with sample values.
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	sales SWORD 500, -250, 400, -100, 350, -150, 700
+
+.code
+main PROC
+
+	exit
+main ENDP
+END main
