a8b9ff238bcc1fb8c920bbfe11483742b2d0c031
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-10-23 01:53:29 +0500

    Create Example07.asm

---
+++ Lab Task 8/Lab Examples/Example07.asm
@@ -0,0 +1,29 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Example 07
+!
+
+INCLUDE Irvine32.inc
+
+.data
+	var1 DWORD 5
+	var2 DWORD 6
+	msg  BYTE "The sum of the two numbers is: ", 0
+
+.code
+main PROC
+	call AddTwo
+	mov edx, OFFSET msg
+	call WriteString
+	call WriteDec
+	call CRLF
+	exit
+main ENDP
+
+AddTwo PROC
+	mov eax, var1
+	add eax, var2
+	ret
+AddTwo ENDP
+
+END main
