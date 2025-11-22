225c25c39d69aca28265c14952c40696f1ccdf56
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-11-22 18:01:31 +0500

    Create Example03.asm

---
+++ Lab Task 11/Lab Examples/Example03.asm
@@ -0,0 +1,26 @@
+; Rayyan Aamir | 24K-0687 | BCS-3F
+COMMENT !
+	Scan for a Matching Character
+!
+INCLUDE Irvine32.inc
+
+.data
+	s1 BYTE "Rayyan",0
+	found BYTE "Letter found!",0
+
+.code
+main PROC
+	mov edi,OFFSET s1
+	mov al,'R'
+	mov ecx,LENGTHOF s1
+	cld
+	repne scasb
+	jnz quit
+	dec edi
+	mov edx, OFFSET found
+	call WriteString
+
+quit:
+	exit
+main ENDP
+END main
